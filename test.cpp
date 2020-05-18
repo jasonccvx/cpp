ec30523f2ab60652d4ed312d7b96d996033fc6b0
jasonccvx <jasonccvx@foxmail.com>
2020-05-19 00:23:04 +0800

    first init

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+    int a=1;
+    for(int i=1;i<5;++i)
+    {
+        cout<<i++<<endl;
+    }
+    system("pause");
+    return 0;
+}
